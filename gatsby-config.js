49fe0aaacbb2cda7b0119d2c67b1437b3c40547f
Pablo Ramirez <pabloramirez@192.168.1.5>
2022-07-21 11:41:32 -0500

    add google analytics 4 config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,27 @@ module.exports = {
     siteUrl: `http://localhost:8000`
   },
   plugins: [
+    {
+        resolve: `gatsby-plugin-google-gtag`,
+        options: {
+          // You can add multiple tracking ids and a pageview event will be fired for all of them.
+          trackingIds: [
+            "G-N9SHECH2QH"// Google Analytics / GA
+            ],
+          // This object gets passed directly to the gtag config command
+          // This config will be shared across all trackingIds
+          gtagConfig: {
+            optimize_id: "OPT_CONTAINER_ID",
+            anonymize_ip: true,
+            cookie_expires: 0,
+          },
+          // This object is used for configuration specific to this plugin
+          pluginConfig: {
+            // Puts tracking script in the head instead of the body
+            head: true
+          },
+        },
+      },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
@@ -30,21 +51,9 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/icon.png`
       }
-    },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    {
-        resolve: `gatsby-plugin-google-gtag`,
-        options: {
-          trackingIds: [
-          "G-N9SHECH2QH", // Google Analytics / GA
-          ],
-          pluginConfig: {
-            // Puts tracking script in the head instead of the body
-            head: true
-          },
-        },
-      }
   ]
 };
