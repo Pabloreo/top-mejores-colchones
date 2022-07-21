c28dd2ee796021843cc54eae650829676f34b94c
Pablo Ramirez <pabloramirez@192.168.1.5>
2022-07-21 11:25:27 -0500

    add google analytics 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,9 +30,21 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/icon.png`
       }
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+        resolve: `gatsby-plugin-google-gtag`,
+        options: {
+          trackingIds: [
+          "YOUR GA-TRACKING_ID", // Google Analytics / GA
+          ],
+          pluginConfig: {
+            // Puts tracking script in the head instead of the body
+            head: true
+          },
+        },
+      }
   ]
 };
